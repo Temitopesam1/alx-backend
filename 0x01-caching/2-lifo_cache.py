c0246e100943720bda2c4a8146400fcefb24ebf4
Temitopesam1 <temitopesam5@gmail.com>
2023-04-19 19:30:02 +0000

    feat: Demonstrate caching system in Python

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ A caching system
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item is not None:
+            if (key not in self.cache_data.keys()) and \
+                    (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
+                print(f'DISCARD: {list(self.cache_data)[-1]}')
+                self.cache_data.popitem()
+            self.cache_data[key] = item
+        return
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        try:
+            return self.cache_data[key]
+        except Exception:
+            return None

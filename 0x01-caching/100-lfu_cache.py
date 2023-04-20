e401c90f0a5e65d78e0c958e4f65e24bc817bee1
Temitopesam1 <temitopesam5@gmail.com>
2023-04-20 21:24:03 +0000

    fix: update 100-lfu_cache.py to shorter and more effective algorithm

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -9,19 +9,16 @@ class LFUCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
-        self.age_dict = {}
         self.freq_dict = {}
         self.age = 1
 
-    def update(self, dict1, dict2, key):
+    def update(self, dictionary, key):
         """ Update dictionary
         """
-        dict1[key] = self.age
-        self.age += 1
-        if key not in dict2.keys():
-            dict2[key] = 1
+        if key not in dictionary.keys():
+            dictionary[key] = 1
         else:
-            dict2[key] += 1
+            dictionary[key] += 1
 
     def least_used_key(self, dictionary):
         """ Get least used key in dictionary
@@ -37,7 +34,6 @@ class LFUCache(BaseCaching):
         """
         print(f'DISCARD: {key}')
         self.cache_data.pop(key)
-        self.age_dict.pop(key)
         self.freq_dict.pop(key)
 
     def put(self, key, item):
@@ -46,25 +42,16 @@ class LFUCache(BaseCaching):
         if key and item:
             if (key not in self.cache_data.keys()) and \
                     (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
-                least_recently = self.least_used_key(self.age_dict)
-                least_frequently = self.least_used_key(self.freq_dict)
-                if least_recently == least_frequently:
-                    self.key_to_remove(least_recently)
-                else:
-                    least_val = min(self.freq_dict[least_frequently],
-                                    self.age_dict[least_recently])
-                    if self.freq_dict[least_frequently] == least_val:
-                        self.key_to_remove(least_frequently)
-                    else:
-                        self.key_to_remove(least_recently)
+                least_frequence = self.least_used_key(self.freq_dict)
+                self.key_to_remove(least_frequence)
             self.cache_data[key] = item
-            self.update(self.age_dict, self.freq_dict, key)
+            self.update(self.freq_dict, key)
 
     def get(self, key):
         """ Get an item by key
         """
         if key and key in self.cache_data.keys():
-            self.update(self.age_dict, self.freq_dict, key)
+            self.update(self.freq_dict, key)
             return self.cache_data[key]
         else:
             return None

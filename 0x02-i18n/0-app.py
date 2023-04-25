eaef3eb1bfa7c9a0efe4f0acee79fd3d1827f5df
Temi Sam <temitopesam5@gmail.com>
2023-04-25 21:14:05 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,10 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello() -> str:
     """ A function to return HTML template  """
     return render_template('0-index.html')

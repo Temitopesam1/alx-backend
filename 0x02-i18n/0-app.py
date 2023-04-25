1dd4cde12ebe514f48310691702497db40db1d6a
Temi Sam <temitopesam5@gmail.com>
2023-04-25 22:07:10 +0100

    fix: rename 0-app.py function

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def get_index() -> str:
+def hello() -> str:
     """ A function to return HTML template  """
     return render_template('0-index.html')
 

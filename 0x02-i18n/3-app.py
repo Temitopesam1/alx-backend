2137d79235357feb621b5702e0b7a134c483bb7b
Temitopesam1 <temitopesam5@gmail.com>
2023-04-25 15:46:01 +0000

    feat: parameterize template/3-index.html file

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script of a flask framework project """
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+from flask import request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ config class for flask app """
+    LANGUAGES = ['en', 'fr']
+
+
+@babel.localeselector
+def get_locale():
+    """ function to get locale """
+    return request.accept_languages.best_match(Config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ A function to return HTML template """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)

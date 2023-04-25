50b94cc2766abda3785775add2303be3fab5ea41
Temitopesam1 <temitopesam5@gmail.com>
2023-04-25 12:44:32 +0000

    feat: Explore flask_babel flask extension

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script of a flask framework project """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config(object):
+    """ config class for flask app """
+    LANGUAGES = ['en', 'fr']
+
+
+@babel.localeselector
+def get_locale():
+    """ function to get locale """
+    return request.accept_languages.best_match(Config['LANGUAGES'])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)

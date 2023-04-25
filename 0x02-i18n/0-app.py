97f6af8c589aefe8a7c82dd9d2de3d4183ad1411
Temi Sam <temitopesam5@gmail.com>
2023-04-25 21:11:03 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,37 +1,36 @@
-# #!/usr/bin/python3
-# """ Script of a flask framework project """
+#!/usr/bin/python3
+""" Script of a flask framework project """
 
-# from flask import Flask, render_template
-
-
-# app = Flask(__name__)
-
-
-# @app.route("/", strict_slashes=False)
-# def hello() -> str:
-#     """ A function to return HTML template  """
-#     return render_template('0-index.html')
-
-
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=5000, debug=True)
-
-#!/usr/bin/env python3
-"""A Basic Flask app.
-"""
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
+@app.route("/", strict_slashes=False)
+def hello() -> str:
+    """ A function to return HTML template  """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
+# #!/usr/bin/env python3
+# """A Basic Flask app.
+# """
+# from flask import Flask, render_template
+
+
+# app = Flask(__name__)
+# app.url_map.strict_slashes = False
+
+
+# @app.route('/')
+# def get_index() -> str:
+#     """The home/index page.
+#     """
+#     return render_template('0-index.html')
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)

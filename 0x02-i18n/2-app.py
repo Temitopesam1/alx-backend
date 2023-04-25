de3d4aee9bb5751909e994e3775d7302245c47c8
Temi Sam <temitopesam5@gmail.com>
2023-04-26 00:46:44 +0100

    Fix: Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,6 +5,9 @@ from flask import Flask, render_template
 from flask_babel import Babel
 from flask import request
 
+app = Flask(__name__)
+babel = Babel(app)
+
 
 class Config:
     """ config class for flask app """
@@ -12,9 +15,6 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-app = Flask(__name__)
-babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:

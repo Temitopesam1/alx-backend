f12265a5234e8fa507f13fda32b43351699f900b
Temitopesam1 <temitopesam5@gmail.com>
2023-04-25 23:17:47 +0000

    fix: update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 """ Script of a flask framework project """
+
 from flask import Flask, render_template
 from flask_babel import Babel
-
-
-class Config:
-    """ config class for flask app """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
+class Config(object):
+    """ config class for flask app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 @app.route("/", strict_slashes=False)
 def hello() -> str:
     """ A function to return HTML template """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)

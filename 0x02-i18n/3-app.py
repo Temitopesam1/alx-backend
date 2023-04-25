e2b2496dd8384689a540fc0f42b77b1e72101a75
Temi Sam <temitopesam5@gmail.com>
2023-04-26 00:20:41 +0100

    Fix: Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,7 +25,7 @@ def get_locale() -> str:
 @app.route("/", strict_slashes=False)
 def hello() -> str:
     """ A function to return HTML template """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':

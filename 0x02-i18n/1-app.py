11e7e998dfa8f97adaf2718501168ae2477fca0c
Temi Sam <temitopesam5@gmail.com>
2023-04-25 21:53:08 +0100

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,51 +6,22 @@ from flask_babel import Babel
 
 class Config:
     """ config class for flask app """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCAL = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@app.route("/", strict_slashes=False)
-def get_index() -> str:
+@app.route("/")
+def hello() -> str:
     """ A function to return HTML template """
     return render_template('1-index.html')
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-# #!/usr/bin/env python3
-# """A Basic Flask app.
-# """
-# from flask_babel import Babel
-# from flask import Flask, render_template
-
-
-# class Config:
-#     """Represents a Flask Babel configuration.
-#     """
-#     LANGUAGES = ["en", "fr"]
-#     BABEL_DEFAULT_LOCALE = "en"
-#     BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# app.url_map.strict_slashes = False
-# babel = Babel(app)
-
-
-# @app.route('/')
-# def get_index() -> str:
-#     """The home/index page.
-#     """
-#     return render_template('1-index.html')
-
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)

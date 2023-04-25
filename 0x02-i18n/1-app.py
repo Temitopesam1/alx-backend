194630c2b0d1a557ff40e193e8880767a9d46627
Temitopesam1 <temitopesam5@gmail.com>
2023-04-25 13:17:21 +0000

    fix: fix bug in 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,10 +6,6 @@ from flask_babel import Babel
 from flask import request
 
 
-app = Flask(__name__)
-babel = Babel(app, locale_selector=get_locale, timezone_selector=get_timezone)
-
-
 class Config(object):
     """ config class for flask app """
     LANGUAGES = ['en', 'fr']
